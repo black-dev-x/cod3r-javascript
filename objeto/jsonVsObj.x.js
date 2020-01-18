c8d76767791c6dfd10f5fe30936fac30581de9e6
thiago.dev.x <thiago.dev.x@gmail.com>
2020-01-18 15:30:54 -0300

    Aula 101, conversões de json e de objetos com stringify e parse

---
+++ objeto/jsonVsObj.x.js
@@ -0,0 +1,21 @@
+//exemplo, você esta criando uma loja e o seu cliente digita o cep
+//você pode terminar de preencher para seu cliente os dados do endereço
+//acesse o endereço viacep.com.br/ws/01001000/json/
+//veja que o retorno é um texto
+const textoRetornadoDoCep01001000 = `{
+    "cep": "01001-000",
+    "logradouro": "Praça da Sé",
+    "complemento": "lado ímpar",
+    "bairro": "Sé",
+    "localidade": "São Paulo",
+    "uf": "SP",
+    "unidade": "",
+    "ibge": "3550308",
+    "gia": "1004"
+  }`
+  const endereco = JSON.parse(textoRetornadoDoCep01001000)
+  console.log(`O endereço do cep ${endereco.cep} fica em ${endereco.localidade}/${endereco.uf}`)
+  //acessar alguma coisa direto do texto não funciona, pois o JSON é simplesmente um texto
+  console.log(textoRetornadoDoCep01001000.cep) 
+  //cep no caso é undefined, pois o texto não tem esse atributo
+  //pode inclusive ter uma exceção dependendo do que você tentar acessar
