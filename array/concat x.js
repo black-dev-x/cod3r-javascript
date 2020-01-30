224b6714356eb6e26ed6e5bb057cb7f484e6f930
thiagodevx <thiago.dev.x@gmail.com>
2020-01-30 13:37:27 -0300

    Aula 118

---
+++ array/concat x.js
@@ -0,0 +1,10 @@
+const roboSolitario = 'Beyoncê'
+
+const duplaDoInferno = ['Sandy', 'Junior']
+
+const moradoresDoAcre = ['Lysk'] //obviamente só tem um
+
+const flopadorasDoPop = [['Lady Gaga','Katy Perry'],['Financiadas pela Pan']]
+const seresEndiabrados = [].concat(roboSolitario, duplaDoInferno, moradoresDoAcre, flopadorasDoPop)
+console.log(seresEndiabrados)
+
