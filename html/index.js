65b8870883eaf0d51ca69ed86dd8b10f29423a41
thiagodevx <thiago.dev.x@gmail.com>
2020-02-28 08:06:17 -0300

    updates visuais

---
+++ html/index.js
@@ -0,0 +1,14 @@
+const clicarNoLink = evento => {
+    evento.preventDefault() //não permite redirecionar
+    const conteudo = document.querySelector('#conteudo')
+    const link = evento.target //o alvo do evento é o link, a tag 'a'
+    const href = link.href //href é a propriedade que fala onde esta o arquivo
+    fetch(href).then(resposta => resposta.text()).then(html => conteudo.innerHTML = html)
+    //esse código acima, não se preocupe tanto com ele, se um dia vc tiver que fazer na mão
+    //vai ser dessa forma, mas já existem formas mais fáceis (como sempre)
+}
+
+const listaDeLinks = document.querySelectorAll('a')
+listaDeLinks.forEach(link => {
+    link.onclick = clicarNoLink
+})
--- html/promise.js
+++
@@ -1,6 +0,0 @@
-async function criaPromise() {
-    const objeto = { nome: 'objeto em uma promise' }
-    return objeto
-}
-
-const promise = criaPromise()
